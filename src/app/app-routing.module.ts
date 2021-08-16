7c99d77cbebd773bf20048c11524e3da3aa44179
Modibo <modiboshs@gmail.com>
2021-08-16 15:52:34 -0700

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { TableComponent } from './page/table/table.component';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'table', component: TableComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
